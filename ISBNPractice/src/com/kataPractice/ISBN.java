0fbec5ae6db5129a7a8eb32a1ec2d6476cc5e929
方嘉慈 <fangjc@sdc.icbc.com.cn>
2014-12-29 15:35:47 +0800

    实现代码让not13DigitTest Pass

--- ISBNPractice/src/com/kataPractice/ISBN.java
+++ ISBNPractice/src/com/kataPractice/ISBN.java
@@ -3,14 +3,18 @@ package com.kataPractice;
 public class ISBN {
 
 	public static boolean isVaildISBN(String isbn) {
+		if (isbn.length() != 13)
+			return false;
+
 		int summing = 0;
 		for (int i = 0; i < 12; i = i + 2) {
 			summing = summing + Integer.parseInt(isbn.substring(i, i + 1));
-			summing = summing + Integer.parseInt(isbn.substring(i + 1, i + 2)) * 3;
+			summing = summing + Integer.parseInt(isbn.substring(i + 1, i + 2))
+					* 3;
 		}
 		int checkDigit = (10 - summing % 10) % 10;
 		if (checkDigit == Integer.parseInt(isbn.substring(12, 13)))
-				return true;
+			return true;
 		return false;
 	}
 
