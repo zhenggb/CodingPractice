60d851c8247906ede4443d26a6b18f11c9f9557a
方嘉慈 <fangjc@sdc.icbc.com.cn>
2014-12-30 16:34:14 +0800

    notVaildDigitTest Pass

--- ISBNPractice/src/com/kataPractice/ISBN.java
+++ ISBNPractice/src/com/kataPractice/ISBN.java
@@ -1,21 +1,25 @@
 package com.kataPractice;
 
+
 public class ISBN {
 
 	public static boolean isVaildISBN(String isbn) {
 		if (isbn.length() != 13)
 			return false;
-
-		int summing = 0;
-		for (int i = 0; i < 12; i = i + 2) {
-			summing = summing + Integer.parseInt(isbn.substring(i, i + 1));
-			summing = summing + Integer.parseInt(isbn.substring(i + 1, i + 2))
-					* 3;
+		try {
+			int summing = 0;
+			for (int i = 0; i < 12; i = i + 2) {
+				summing = summing + Integer.parseInt(isbn.substring(i, i + 1));
+				summing = summing + Integer.parseInt(isbn.substring(i + 1, i + 2)) * 3;
+			}
+			int checkDigit = (10 - summing % 10) % 10;
+			if (checkDigit == Integer.parseInt(isbn.substring(12, 13)))
+				return true;
+		} catch (NumberFormatException e) {
+			return false;
 		}
-		int checkDigit = (10 - summing % 10) % 10;
-		if (checkDigit == Integer.parseInt(isbn.substring(12, 13)))
-			return true;
 		return false;
 	}
+
 
 }
