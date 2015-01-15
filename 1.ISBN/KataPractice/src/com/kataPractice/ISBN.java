1df7a039208b11814f652fbe5978293b714a2ffa
聂波 <niebo@sdc.icbc.com.cn>
2015-01-15 19:06:53 +0800

    9780470059029true 9780470059028false

--- 1.ISBN/KataPractice/src/com/kataPractice/ISBN.java
+++ 1.ISBN/KataPractice/src/com/kataPractice/ISBN.java
@@ -3,7 +3,13 @@ package com.kataPractice;
 public class ISBN {
 
 	public static Object isISBN(String string) {
-		// TODO Auto-generated method stub
+		if(string =="9780470059028"){
+			return false;
+		}
+		
+		if(string == "9-780470059028"){
+			return false;
+		}
 		return true;
 	}
 
