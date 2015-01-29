064432d52693c960a28db017b511ffc344220fce
孔维涛 <kongwt@sdc.icbc.com.cn>
2015-01-29 19:03:00 +0800

    测试未成功

--- 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
@@ -5,11 +5,15 @@ public class ISBN {
 	public static boolean verify(String ISBNstr) {
 		int sum = 0;
 		for(int i=0;i<ISBNstr.length();i=i+2){
-			sum += (ISBNstr.charAt(i)-41)*1+(ISBNstr.charAt(i+1)-41)*3;
+			int a = Integer.parseInt(Character.toString( ISBNstr.charAt(i)));
+			int b = Integer.parseInt(Character.toString( ISBNstr.charAt(i + 1)));
+			sum += a*1+ b*3;
 			
 		}
+		System.out.print(sum);
 		sum = (10-sum%10)%10;
-		if(sum==(ISBNstr.charAt(13)-41))
+		System.out.print(sum);
+		if(sum==Integer.parseInt(Character.toString (ISBNstr.charAt(13))))
 				return true;
 		else
 			 return false;
