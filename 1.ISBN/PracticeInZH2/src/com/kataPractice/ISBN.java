89d52684572e582034efdc3a0ddb1b0e8b64522e
欧建斌 <oujb@sdc.icbc.com.cn>
2015-01-29 19:24:08 +0800

    完美实现了13位校验位

--- 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
@@ -2,27 +2,21 @@ package com.kataPractice;
 
 public class ISBN {
 
-	public static boolean verify(String ISBNstr) {
+	public static boolean verify(String isbnCode) {
 		int sum = 0;
-		
-		System.out.println(ISBNstr.length());
-		
-		for(int i=0;i<ISBNstr.length()-1;i=i+2){
-			int a = Integer.parseInt(Character.toString( ISBNstr.charAt(i)));
-			int b = Integer.parseInt(Character.toString( ISBNstr.charAt(i + 1)));
-			
-			System.out.println("a:"+a+"|b:"+b);
+		int checkBit = 0;
+		for(int i=0;i<11;i=i+2){
+			int a = Integer.parseInt(Character.toString( isbnCode.charAt(i)));
+			int b = Integer.parseInt(Character.toString( isbnCode.charAt(i + 1)));
 			
 			sum += a*1+ b*3;
-			
 		}
-		System.out.println(sum);
-		sum = (10-sum%10)%10;
-		System.out.println(sum);
-		if(sum==Integer.parseInt(Character.toString (ISBNstr.charAt(13))))
-				return true;
+
+		checkBit = (10-sum%10)%10;
+		if(checkBit==Integer.parseInt(Character.toString (isbnCode.charAt(12))))
+			return true;
 		else
-			 return false;
+			return false;
 		
 	}
 
