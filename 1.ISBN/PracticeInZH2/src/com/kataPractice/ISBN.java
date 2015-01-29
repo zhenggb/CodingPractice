4533eb176bb76d7200ea40904d7e8340678044cd
方嘉慈 <fangjc@sdc.icbc.com.cn>
2015-01-29 19:32:05 +0800

    实现忽略包含空格和短横线

--- 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
@@ -3,21 +3,27 @@ package com.kataPractice;
 public class ISBN {
 
 	public static boolean verify(String isbnCode) {
+		isbnCode = isbnCode.replaceAll(" ", "");
+		isbnCode = isbnCode.replaceAll("-", "");
 		int sum = 0;
 		int checkBit = 0;
 		for(int i=0;i<11;i=i+2){
-			int a = Integer.parseInt(Character.toString( isbnCode.charAt(i)));
-			int b = Integer.parseInt(Character.toString( isbnCode.charAt(i + 1)));
+			int a = getOneNumber(isbnCode, i);
+			int b = getOneNumber(isbnCode, i+1);
 			
 			sum += a*1+ b*3;
 		}
 
 		checkBit = (10-sum%10)%10;
-		if(checkBit==Integer.parseInt(Character.toString (isbnCode.charAt(12))))
+		if(checkBit==getOneNumber(isbnCode, 12))
 			return true;
 		else
 			return false;
 		
+	}
+
+	private static int getOneNumber(String isbnCode, int i) {
+		return Integer.parseInt(Character.toString( isbnCode.charAt(i)));
 	}
 
 }
--- 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBNTest.java
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBNTest.java
@@ -15,9 +15,14 @@ public class ISBNTest {
 		assertEquals("验证9780470059028是失败", false, ISBN.verify("9780470059028"));
 	}
 	
-	public void 验证9780471486480是成功() {
-		assertEquals("验证9780471486480是成功的", true, ISBN.verify("978 0 471 48648 0"));
+	@Test
+	public void 验证带空格的是成功(){
+		assertEquals(true, ISBN.verify("978 047 0059 029"));
 	}
 	
+	@Test
+	public void 验证带横线的是成功(){
+		assertEquals(true, ISBN.verify("978-047-0059-029"));
+	}
 
 }
