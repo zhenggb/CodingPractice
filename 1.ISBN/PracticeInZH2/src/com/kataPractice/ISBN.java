924ca5c5108496f40d48b1509597144553d639a1
郑广斌 <zhenggb@sdc.icbc.com.cn>
2015-01-29 18:46:48 +0800

    新增两个案例

---
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
@@ -0,0 +1,9 @@
+package com.kataPractice;
+
+public class ISBN {
+
+	public static boolean verify(String ISBNstr) {
+		return true;
+	}
+
+}
--- 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBNTest.java
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBNTest.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 public class ISBNTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void 验证9780470059029是成功() {
+		assertEquals("验证9780470059029是成功的", true, ISBN.verify("9780470059029"));
 	}
+	
+	@Test
+	public void 验证9780471486480是成功() {
+		assertEquals("验证9780471486480是成功的", true, ISBN.verify("978 0 471 48648 0"));
+	}
+	
 
 }
