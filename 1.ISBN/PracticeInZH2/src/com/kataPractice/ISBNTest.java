f16f6bcdb2a9d6066c28836b9d5bd481299283e5
方嘉慈 <fangjc@sdc.icbc.com.cn>
2015-01-29 16:49:47 +0800

    项目初始化

---
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBNTest.java
@@ -0,0 +1,14 @@
+package com.kataPractice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ISBNTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
