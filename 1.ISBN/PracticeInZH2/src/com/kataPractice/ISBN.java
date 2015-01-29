e09ab5952d2330b40698160686bca3bf8bb12ec8
罗琦山 <luoqs@sdc.icbc.com.cn>
2015-01-29 18:55:10 +0800

    13位计算校验位，未成功测试

--- 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
+++ 1.ISBN/PracticeInZH2/src/com/kataPractice/ISBN.java
@@ -3,7 +3,17 @@ package com.kataPractice;
 public class ISBN {
 
 	public static boolean verify(String ISBNstr) {
-		return true;
+		int sum = 0;
+		for(int i=0;i<ISBNstr.length();i=i+2){
+			sum += (ISBNstr.charAt(i)-41)*1+(ISBNstr.charAt(i+1)-41)*3;
+			
+		}
+		sum = (10-sum%10)%10;
+		if(sum==(ISBNstr.charAt(13)-41))
+				return true;
+		else
+			 return false;
+		
 	}
 
 }
