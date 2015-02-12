599926228d937505ce758449d232c312d8fb16ce
何小明 <hexm01@sdc.icbc.com.cn>
2015-02-12 19:54:32 +0800

    把横赢的情况修改为循环判断

--- 3.JING/src/jing/Gamer.java
+++ 3.JING/src/jing/Gamer.java
@@ -3,58 +3,21 @@ package jing;
 public class Gamer {
 
 	public static Object getResult(String[][] game) {
-
 		try{
-			if(checkHeng(game,0,"X"))
-				return "X";
-			if(checkHeng(game,1,"X"))
-				return "X";
+			for(int i=0; i < 3; i++) {
+				if(checkHeng(game,i,"X")){
+					return "X";
+				}
+			}
 			if(checkSu(game,0,"X"))
 				return "X";
 			if(checkXie(game,"X"))
 				return "X";
 			if(checkXie(game,"O"))
 				return "O";
-			int Xsum = 0;
-			int Osum = 0;
-
-			for (int i = 0; i < 3; i++) {
-				int xCount = 0;
-				int oCount = 0;
-				for (int j = 0; j < 3; j++) {
-					String character = game[i][j];
-					if ("X".equals(character)) {
-						xCount += 1;
-						Xsum++;
-					}else if ("O".equals(character)) {
-						oCount += 1;
-						Osum++;
-					}else{
-						return "Error";
-					}
-				}
-
-				if (xCount == 3) {
-					if (Xsum - Osum == 0 || Xsum - Osum == -1 || Xsum - Osum == 1) {
-						return "-";
-					}
-					return "X";
-				}
-
-				if (oCount == 3) {
-					if (xCount == 3) {
-						if (Xsum - Osum == 0 || Xsum - Osum == -1
-								|| Xsum - Osum == 1) {
-							return "-";
-						}
-						return "O";
-					}
-				}
-			}
 		}catch(Exception e){
 			return "Error";
 		}
-		
 		return "Error";
 	}
 	private static boolean checkHeng(String[][] game,int i,String flag){
