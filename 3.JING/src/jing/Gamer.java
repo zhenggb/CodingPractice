fa20b6dd12bfc531168a381260cb58ee85e45d1c
郑广斌 <zhenggb@sdc.icbc.com.cn>
2015-02-12 18:49:17 +0800

    增加一个判断O胜利的案例

--- 3.JING/src/jing/Gamer.java
+++ 3.JING/src/jing/Gamer.java
@@ -6,6 +6,21 @@ public class Gamer {
 		if(game[0][0].equals("X")&&game[0][1].equals("X")&&game[0][2].equals("X")){
 			return "X";
 		}
+		if(game[1][0].equals("O")&&game[1][1].equals("O")&&game[1][2].equals("O")){
+			return "O";
+		}
+		int total_x=0;
+		int total_o=0;
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+			
+				if(game[i][j].equals("O")){
+					total_o++;
+				}else if(game[i][j].equals("X")){
+					total_x++;
+				}
+			}
+		}
 		return null;
 	}
 
--- 3.JING/src/jing/TestJing.java
+++ 3.JING/src/jing/TestJing.java
@@ -15,4 +15,22 @@ public class TestJing {
 		};
 		Assert.assertSame("X", Gamer.getResult(game));
 	}
+	@Test
+	public void testO(){
+		String[][] game = new String[][]{
+				{"X","X","_"},
+				{"O","O","O"},
+				{"_","_","_"},
+		};
+		Assert.assertSame("O", Gamer.getResult(game));
+	}
+	@Test
+	public void testError(){
+		String[][] game = new String[][]{
+				{"X","X","O"},
+				{"O","O","O"},
+				{"_","_","_"},
+		};
+		Assert.assertSame("Error", Gamer.getResult(game));
+	}
 }
