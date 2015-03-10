6cffd379e7b571419f046f66a44d0d7952128b6c
巫昌凯 <wuck@sdc.icbc.com.cn>
2015-03-10 19:02:56 +0800

    完成了一半的逻辑

--- 3.JING/src/jing/Chess.java
+++ 3.JING/src/jing/Chess.java
@@ -3,6 +3,21 @@ package jing;
 public class Chess {
 
 	public String check(String[][] chessboard) {
+		int XCount=0;
+		int OCount=0;
+		for(int i=0;i<=chessboard.length;i++){
+			
+		  if(chessboard[0][i]==chessboard[1][i]&&chessboard[0][i]==chessboard[2][i]){
+		    if(chessboard[0][i]=="X"){
+		    	XCount++;
+		    }
+		    if(chessboard[0][i]=="O"){
+		    	OCount++;
+		    }
+		  }
+		}
+		
+		 
 		return "X";
 	}
 
