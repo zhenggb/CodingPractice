6772a786d7cd319d653504f0c1e3ecea4710020c
方嘉慈 <fangjc@sdc.icbc.com.cn>
2015-03-10 09:31:10 +0800

    pass isVaildISBN10Test()

--- 1.ISBN/ISBNPractice/src/com/kataPractice/ISBN.java
+++ 1.ISBN/ISBNPractice/src/com/kataPractice/ISBN.java
@@ -10,21 +10,33 @@ public class ISBN {
 	public boolean isVaildISBN() {
 		isbn = isbn.replaceAll(" ", "");
 		isbn = isbn.replaceAll("-", "");
-		if (isbn.length() != 13)
+		if (isbn.length() != 13 && isbn.length() != 10)
 			return false;
 		try {
-			int summing = 0;
-			for (int i = 0; i < 12; i = i + 2) {
-				summing = summing + getDigitByPosition(i);
-				summing = summing + getDigitByPosition(i + 1) * 3;
-			}
-			int checkDigit = (10 - summing % 10) % 10;
-			if (checkDigit == getDigitByPosition(12))
+			if (getCheckDigit() == getDigitByPosition(isbn.length() - 1))
 				return true;
 		} catch (NumberFormatException e) {
 			return false;
 		}
 		return false;
+	}
+
+	private int getCheckDigit() {
+		if (isbn.length() == 13) {
+			int summing = 0;
+			for (int i = 0; i < 12; i = i + 2) {
+				summing = summing + getDigitByPosition(i);
+				summing = summing + getDigitByPosition(i + 1) * 3;
+			}
+			return (10 - summing % 10) % 10;
+		}else if(isbn.length() == 10){
+			int summing = 0;
+			for (int i =0; i < 9; i = i +1){
+				summing = summing + (i+1)*getDigitByPosition(i);
+			}
+			return summing % 11;
+		}
+		return -1;
 	}
 
 	private int getDigitByPosition(int position) {
