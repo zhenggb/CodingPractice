1b81a8200e6ea46725d65313505ccb5f81a767c4
郑广斌 <zhenggb@sdc.icbc.com.cn>
2015-03-10 19:21:15 +0800

    实现了竖全X赢

--- 3.JING/src/jing/Chess.java
+++ 3.JING/src/jing/Chess.java
@@ -3,13 +3,19 @@ package jing;
 public class Chess {
 
 	public String check(String[][] chessboard) {
-		if (chessboard[0][0] == "X" && chessboard[0][1] == "X"
-				&& chessboard[0][2] == "X") {
-			return "X";
+
+		for (int i = 0; i < 3; i++) {
+			if (chessboard[i][0] == "X" && chessboard[i][1] == "X"
+					&& chessboard[i][2] == "X") {
+				return "X";
+			}
 		}
-		if (chessboard[1][0] == "X" && chessboard[1][1] == "X"
-				&& chessboard[1][2] == "X") {
-			return "X";
+		
+		for (int j = 0; j < 3; j++) {
+			if (chessboard[0][j] == "X" && chessboard[1][j] == "X"
+					&& chessboard[2][j] == "X") {
+				return "X";
+			}
 		}
 		return "-";
 	}
--- 3.JING/src/jing/ChessTest.java
+++ 3.JING/src/jing/ChessTest.java
@@ -5,23 +5,32 @@ import org.junit.Test;
 
 public class ChessTest {
 	@Test
-	public void testHeng1(){
-		String[][] chessboard = new String[][]{
-				{"X","X","X"},
-				{"_","O","_"},
-				{"O","_","_"}
-			};
-		
-		Assert.assertSame("X", new Chess().check(chessboard));		
+	public void testHeng1() {
+		String[][] chessboard = new String[][] {
+				{ "X", "X", "X" },
+				{ "_", "O", "_" }, 
+				{ "O", "_", "_" } };
+
+		Assert.assertSame("X", new Chess().check(chessboard));
 	}
+
 	@Test
-	public void testHeng2(){
-	String[][] chessboard = new String[][]{
-			{"_","O","_"},
-			{"X","X","X"},
-			{"O","_","_"}
-		};
+	public void testHeng2() {
+		String[][] chessboard = new String[][] { 
+				{ "_", "O", "_" },
+				{ "X", "X", "X" }, 
+				{ "O", "_", "_" } };
+
+		Assert.assertSame("X", new Chess().check(chessboard));
+	}
 	
-	Assert.assertSame("X", new Chess().check(chessboard));		
-}
+	@Test
+	public void testshu1() {
+		String[][] chessboard = new String[][] {
+				{ "X", "O", "_" },
+				{ "X", "O", "O" }, 
+				{ "X", "_", "_" } };
+
+		Assert.assertSame("X", new Chess().check(chessboard));
+	}
 }
