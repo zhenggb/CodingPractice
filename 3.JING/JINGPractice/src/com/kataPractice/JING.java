e1f520e310820370d97826c7eae05f77e0bdd2e7
方嘉慈 <fangjc@sdc.icbc.com.cn>
2015-03-11 15:27:10 +0800

    pass shuWin and xieWin

--- 3.JING/JINGPractice/src/com/kataPractice/JING.java
+++ 3.JING/JINGPractice/src/com/kataPractice/JING.java
@@ -8,6 +8,20 @@ public class JING {
 				return chessBoard[i][0];
 			}
 		}
+		for (int i = 0; i < 3; i++) {
+			if (chessBoard[0][i] == chessBoard[1][i]
+					&& chessBoard[0][i] == chessBoard[2][i]) {
+				return chessBoard[0][i];
+			}
+		}
+		if (chessBoard[0][0] == chessBoard[1][1]
+				&& chessBoard[1][1] == chessBoard[2][2]) {
+			return chessBoard[1][1];
+		}
+		if (chessBoard[0][2] == chessBoard[1][1]
+				&& chessBoard[1][1] == chessBoard[2][0]) {
+			return chessBoard[1][1];
+		}
 		return "-";
 	}
 }
--- 3.JING/JINGPractice/src/com/kataPractice/JINGTest.java
+++ 3.JING/JINGPractice/src/com/kataPractice/JINGTest.java
@@ -33,4 +33,44 @@ public class JINGTest {
 		};
 		assertEquals("O",new JING().check(chessBoard));
 	}
+	
+	@Test
+	public void firstShuOWinTest(){
+		String[][] chessBoard ={
+				{"O", "X", "_"},
+				{"O", "X", "_"},
+				{"O", "_", "_"}
+		};
+		assertEquals("O",new JING().check(chessBoard));
+	}
+	
+	@Test
+	public void thirdShuOWinTest(){
+		String[][] chessBoard ={
+				{"X", "X", "O"},
+				{"_", "X", "O"},
+				{"_", "_", "O"}
+		};
+		assertEquals("O",new JING().check(chessBoard));
+	}
+	
+	@Test
+	public void xieXWinTest(){
+		String[][] chessBoard ={
+				{"X", "O", "O"},
+				{"_", "X", "O"},
+				{"_", "_", "X"}
+		};
+		assertEquals("X",new JING().check(chessBoard));
+	}
+	
+	@Test
+	public void xieReverseXWinTest(){
+		String[][] chessBoard ={
+				{"_", "O", "X"},
+				{"_", "X", "O"},
+				{"X", "_", "_"}
+		};
+		assertEquals("X",new JING().check(chessBoard));
+	}
 }
