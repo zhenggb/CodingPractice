2a165ee44e5c2e7debef4f3e2b961a17adb3e2f5
方嘉慈 <fangjc@sdc.icbc.com.cn>
2015-03-11 15:52:20 +0800

    重构

--- 3.JING/JINGPractice/src/com/kataPractice/JING.java
+++ 3.JING/JINGPractice/src/com/kataPractice/JING.java
@@ -7,21 +7,35 @@ public class JING {
 					&& chessBoard[i][0] == chessBoard[i][2]) {
 				return chessBoard[i][0];
 			}
-		}
-		for (int i = 0; i < 3; i++) {
 			if (chessBoard[0][i] == chessBoard[1][i]
 					&& chessBoard[0][i] == chessBoard[2][i]) {
 				return chessBoard[0][i];
 			}
 		}
-		if (chessBoard[0][0] == chessBoard[1][1]
-				&& chessBoard[1][1] == chessBoard[2][2]) {
-			return chessBoard[1][1];
-		}
-		if (chessBoard[0][2] == chessBoard[1][1]
-				&& chessBoard[1][1] == chessBoard[2][0]) {
+		if (checkXie(chessBoard)||checkXieReverse(chessBoard)) {
 			return chessBoard[1][1];
 		}
 		return "-";
+	}
+
+	private boolean checkXie(String[][] chessBoard) {
+		int xCount = 0;
+		int oCount = 0;
+		int hengStep = 1;
+		int shuStep = 1;
+		for (int i = 0, j = 0; i < 3; i = i + hengStep, j = j + shuStep) {
+			if (chessBoard[i][j].equals("X"))
+				xCount++;
+			if (chessBoard[i][j].equals("O"))
+				oCount++;
+		}
+		if (xCount == 3 || oCount == 3)
+			return true;
+		return false;
+	}
+	
+	private boolean checkXieReverse(String[][] chessBoard) {
+		return chessBoard[0][2] == chessBoard[1][1]
+				&& chessBoard[1][1] == chessBoard[2][0];
 	}
 }
