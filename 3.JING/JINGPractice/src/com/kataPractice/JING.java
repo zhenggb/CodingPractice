0f0bd49eec575276de5745a0171ac0ec5792706a
方嘉慈 <fangjc@sdc.icbc.com.cn>
2015-03-11 15:18:18 +0800

    pass firstHengXWinTest and firstHengOWinTest

--- 3.JING/JINGPractice/src/com/kataPractice/JING.java
+++ 3.JING/JINGPractice/src/com/kataPractice/JING.java
@@ -2,6 +2,12 @@ package com.kataPractice;
 
 public class JING {
 	public String check(String[][] chessBoard) {
-		return "X";
+		for (int i = 0; i < 3; i++) {
+			if (chessBoard[0][0] == chessBoard[0][1]
+					&& chessBoard[0][0] == chessBoard[0][2]) {
+				return chessBoard[0][0];
+			}
+		}
+		return "-";
 	}
 }
--- 3.JING/JINGPractice/src/com/kataPractice/JINGTest.java
+++ 3.JING/JINGPractice/src/com/kataPractice/JINGTest.java
@@ -5,12 +5,22 @@ import org.junit.Test;
 
 public class JINGTest {
 	@Test
-	public void HengWinTest(){
+	public void firstHengXWinTest(){
 		String[][] chessBoard ={
 				{"X", "X", "X"},
 				{"O", "O", "_"},
 				{"_", "_", "_"}
 		};
 		assertEquals("X",new JING().check(chessBoard));
+	}
+	
+	@Test
+	public void firstHengOWinTest(){
+		String[][] chessBoard ={
+				{"O", "O", "O"},
+				{"X", "X", "_"},
+				{"_", "X", "_"}
+		};
+		assertEquals("O",new JING().check(chessBoard));
 	}
 }
