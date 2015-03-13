a5609ca0071e89621b788f56660c505845cdd494
方嘉慈 <fangjc@sdc.icbc.com.cn>
2015-03-13 13:59:18 +0800

    依然是重构

--- 3.JING/JINGPractice/src/com/kataPractice/JING.java
+++ 3.JING/JINGPractice/src/com/kataPractice/JING.java
@@ -6,10 +6,14 @@ public class JING {
 	int oWinCount = 0;
 
 	public String check(String[][] chessBoard) {
-		check(chessBoard, 0, 1);
-		check(chessBoard, 1, 0);
-		check(chessBoard, 1, 1);
-		check(chessBoard, -1, 1);
+		xWinCount = xWinCount + check(chessBoard, "X", 0, 1);
+		xWinCount = xWinCount + check(chessBoard, "X", 1, 0);
+		xWinCount = xWinCount + check(chessBoard, "X", 1, 1);
+		xWinCount = xWinCount + check(chessBoard, "X", -1, 1);
+		oWinCount = oWinCount + check(chessBoard, "O", 0, 1);
+		oWinCount = oWinCount + check(chessBoard, "O", 1, 0);
+		oWinCount = oWinCount + check(chessBoard, "O", 1, 1);
+		oWinCount = oWinCount + check(chessBoard, "O", -1, 1);
 		if (xWinCount == 1 && oWinCount == 0) {
 			setWinner("X");
 		}
@@ -22,21 +26,18 @@ public class JING {
 		return getWinner();
 	}
 
-	private void check(String[][] chessBoard, int hengStep, int shuStep) {
+	private int check(String[][] chessBoard, String chess, int hengStep, int shuStep) {
+		int winCount = 0;
 		for (int startI = 0; startI < 3; startI++) {
 			for (int startJ = 0; startJ < 3; startJ++) {
-				int xCount = 0;
-				int oCount = 0;
+				int count = 0;
 				for (int i = startI, j = startJ; isValidCell(i, j); i = i + hengStep, j = j + shuStep) {
-					if (chessBoard[i][j].equals("X"))
-						xCount++;
-					if (chessBoard[i][j].equals("O"))
-						oCount++;
+					if (chessBoard[i][j].equals(chess)) count++;
 				}
-				if (xCount == 3) xWinCount++;
-				if (oCount == 3) oWinCount++;
+				if (count == 3) winCount++;
 			}
 		}
+		return winCount;
 	}
 
 	private boolean isValidCell(int i, int j) {
