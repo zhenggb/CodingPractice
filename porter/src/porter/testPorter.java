3121a4ce9de0557fd1140228afa61d5199d7b616
kfzx-zhenggb <zhenggb@sdc.icbc.com.cn>
2015-03-18 15:38:09 +0800

    补充一个案例：只买一本哈利波特第4集

--- porter/src/porter/testPorter.java
+++ porter/src/porter/testPorter.java
@@ -15,5 +15,9 @@ public class testPorter {
 	public void test只买一本哈利波特第1集() {
 		assertSame( (long)8,new porter().price(new int[] {0}));
 	}
+	@Test
+	public void test只买一本哈利波特第4集() {
+		assertSame( (long)8,new porter().price(new int[] {3}));
+	}
 
 }
